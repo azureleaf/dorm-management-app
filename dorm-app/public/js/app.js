547e1136d53b394545b47a2d8c3be652cbbb2e63
akb <doge.wow.so.scare@gmail.com>
2020-01-23 22:13:20 +0900

    chore: unknown minor changes

--- dorm-app/public/js/app.js
+++ dorm-app/public/js/app.js
@@ -49845,8 +49845,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/akb/Documents/repos/dorm-management-app/dorm-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/akb/Documents/repos/dorm-management-app/dorm-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/akb/repos/dorm-management-app/dorm-app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/akb/repos/dorm-management-app/dorm-app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
