270add6bab6656d5b5258d5e01bf07a1cf4019cf
akb <doge.wow.so.scare@gmail.com>
2020-01-23 10:48:35 +0900

    feat: Add Vue component to the blade

---
+++ dorm-app/resources/js/components/MembersTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Example Vue Component</h1>
+    <p>I'm an example component.</p>
+    <h2>v-for</h2>
+    <ul>
+      <li v-for="(country, i) in countries" :key="i">
+        Capital city of {{ country.name }} is {{ country.capital }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      countries: [
+        { name: 'China', capital: 'Beijing' },
+        { name: 'Japan', capital: 'Tokyo' },
+        { name: 'Korea', capital: 'Seoul' }
+      ]
+    };
+  },
+  mounted() {
+    console.log('Component mounted.');
+  }
+};
+</script>
--- dorm-app/resources/views/MembersController/member.blade.php
+++ dorm-app/resources/views/MembersController/member.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
     <title>Laravel</title>
     <style>
         #paymentTable {
@@ -19,79 +22,88 @@
 </head>
 
 <body>
-    <h1>寮生名簿</h1>
-    <a href="/">Top</a>
+    <div id="app">
 
-    <div>
-        <h2>名簿編集</h2>
-        <form method="POST" action="/members/add">
-            {{ csrf_field()}}
+        <h1>寮生名簿</h1>
+        <a href="/">Top</a>
 
-            <select name="changeMemberInfo">
-                <option value="default" disabled selected>編集種別を選択</option>
-                <option value="joinDorm">入寮処理</option>
-                <option value="leaveDorm">退寮処理</option>
-                <option value="changeRoom">部屋替え</option>
-                <option value="changeRole">役職修正</option>
-                <option value="changeName">氏名修正</option>
-                <option value="changeJoinDate">入寮日修正</option>
-                <option value="changeLeaveDate">退寮日修正</option>
-            </select>
+        <p>Vue components:</p>
+        <members-table></members-table>
 
-            <input type="text" name="room" placeholder="部屋番号">
-            <input type="text" name="lastName" placeholder="姓">
-            <input type="text" name="firstName" placeholder="名">
-            <input type="text" name="lastNameReading" placeholder="セイ">
-            <input type="text" name="firstNameReading" placeholder="ナマエ">
-
-            <input type="submit" value="変更登録">
-        </form>
-    </div>
-    <div>
-        <h2>寮生一覧</h2>
-        @if (Auth::check())
-        <p>あなたは、{{ $user->name }}さんとしてログインしています。</p>
         <div>
-            <form method="POST" action="/members">
+            <h2>名簿編集</h2>
+            <form method="POST" action="/members/add">
                 {{ csrf_field()}}
-                <input type="checkbox" name="filterCurrentMembers" value="true" checked>在寮者を表示
-                <input type="checkbox" name="filterCurrentMembers" value="true">退寮者を表示
-                <input type="text" name="roomNum" placeholder="部屋番号で絞り込み">
-                <input type="submit" value="表示更新">
+
+                <select name="changeMemberInfo">
+                    <option value="default" disabled selected>編集種別を選択</option>
+                    <option value="joinDorm">入寮処理</option>
+                    <option value="leaveDorm">退寮処理</option>
+                    <option value="changeRoom">部屋替え</option>
+                    <option value="changeRole">役職修正</option>
+                    <option value="changeName">氏名修正</option>
+                    <option value="changeJoinDate">入寮日修正</option>
+                    <option value="changeLeaveDate">退寮日修正</option>
+                </select>
+
+                <input type="text" name="room" placeholder="部屋番号">
+                <input type="text" name="lastName" placeholder="姓">
+                <input type="text" name="firstName" placeholder="名">
+                <input type="text" name="lastNameReading" placeholder="セイ">
+                <input type="text" name="firstNameReading" placeholder="ナマエ">
+
+                <input type="submit" value="変更登録">
             </form>
         </div>
-        @if(count($members)==0)
-        <p>指定された条件に合致する記録はありません。</p>
-        @else
-        <table id="paymentTable">
-            <tr>
-                <th>寮生番号</th>
-                <th>部屋番号</th>
-                <th>氏名</th>
-                <th>ヨミガナ</th>
-                <th>入寮日</th>
-                <th>退寮日</th>
-                <th>履歴</th>
-            </tr>
-            @foreach($members as $member)
-            <tr>
-                <td>{{$member->id}}</td>
-                <td>{{$member->room}}</td>
-                <td>{{$member->last_name}} {{$member->first_name}}</td>
-                <td>{{$member->last_name_reading}} {{$member->first_name_reading}}</td>
-                <td>{{$member->date_join}}</td>
-                <td>{{$member->date_leave}}</td>
-                <td><a href="">個人履歴</a></td>
+        <div>
+            <h2>寮生一覧</h2>
+            @if (Auth::check())
+            <p>あなたは、{{ $user->name }}さんとしてログインしています。</p>
+            <div>
+                <form method="POST" action="/members">
+                    {{ csrf_field()}}
+                    <input type="checkbox" name="filterCurrentMembers" value="true" checked>在寮者を表示
+                    <input type="checkbox" name="filterCurrentMembers" value="true">退寮者を表示
+                    <input type="text" name="roomNum" placeholder="部屋番号で絞り込み">
+                    <input type="submit" value="表示更新">
+                </form>
+            </div>
+            @if(count($members)==0)
+            <p>指定された条件に合致する記録はありません。</p>
+            @else
+            <table id="paymentTable">
+                <tr>
+                    <th>寮生番号</th>
+                    <th>部屋番号</th>
+                    <th>氏名</th>
+                    <th>ヨミガナ</th>
+                    <th>入寮日</th>
+                    <th>退寮日</th>
+                    <th>履歴</th>
+                </tr>
+                @foreach($members as $member)
+                <tr>
+                    <td>{{$member->id}}</td>
+                    <td>{{$member->room}}</td>
+                    <td>{{$member->last_name}} {{$member->first_name}}</td>
+                    <td>{{$member->last_name_reading}} {{$member->first_name_reading}}</td>
+                    <td>{{$member->date_join}}</td>
+                    <td>{{$member->date_leave}}</td>
+                    <td><a href="">個人履歴</a></td>
 
-            </tr>
-            @endforeach
+                </tr>
+                @endforeach
+                @endif
+            </table>
+            @else
+            <p>寮生一覧を見るには、<a href="/register">ログイン</a>してください。</p>
             @endif
-        </table>
-        @else
-        <p>寮生一覧を見るには、<a href="/register">ログイン</a>してください。</p>
-        @endif
+
+        </div>
+
 
     </div>
+    <script src=" {{ mix('js/app.js') }} "></script>
 
 </body>
 
