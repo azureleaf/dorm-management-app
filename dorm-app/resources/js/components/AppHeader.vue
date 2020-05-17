a5b844ffba53e284af0fdc4f3647213b34cbdbbf
akb <doge.wow.so.scare@gmail.com>
2020-05-17 22:28:13 +0900

    feat: Show hamburger menu in the app bar for a small display

--- dorm-app/resources/js/components/AppHeader.vue
+++ dorm-app/resources/js/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-app-bar class="blue darken-4" dense dark app>
-      <v-avatar class="d-none d-md-flex d-lg-flex d-xl-flex">
+      <v-avatar>
         <v-img class="mx-2" :src="avatarurl" max-height="50" max-width="50" contain></v-img>
       </v-avatar>
       <v-toolbar-title class="ml-5 headline" style="font-family: 'Titillium Web' !important;">
@@ -13,17 +13,30 @@
         <v-divider vertical></v-divider>
         <v-btn
           v-for="page in pages"
-          :key="page"
+          :key="page.title"
           text
           :href="page.uri"
-          class="mx-0 px-0"
           :title="page.uri"
+          class="mx-0 px-0 d-none d-sm-flex"
         >
           <v-icon>{{page.icon}}</v-icon>
           <!-- <span class="d-none d-lg-flex d-xl-flex">{{ page.title }}</span> -->
         </v-btn>
         <v-divider vertical></v-divider>
       </v-toolbar-items>
+      <v-menu v-model="isNavOpen" :offsetY="true">
+        <template v-slot:activator="{ on }">
+          <v-app-bar-nav-icon v-on="on" class="d-flex d-sm-none"></v-app-bar-nav-icon>
+        </template>
+        <v-list>
+          <v-list-item v-for="page in pages" :key="page.title" :href="page.uri">
+            <v-list-item-icon class="mr-3">
+              <v-icon v-text="page.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ page.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
   </v-container>
 </template>
@@ -41,7 +54,8 @@ export default {
         { title: "寮生名簿", uri: "/userlist", icon: "mdi-view-list" },
         { title: "寮文書", uri: "/archive", icon: "mdi-file-document" },
         { title: "ログアウト", uri: "/logout", icon: "mdi-lock-open" }
-      ]
+      ],
+      isNavOpen: false
     };
   },
   mounted: function() {
