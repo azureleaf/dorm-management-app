57947b8e56d4273f3a0ff7b4b248ca49ec90ad13
akb <doge.wow.so.scare@gmail.com>
2020-05-17 21:48:59 +0900

    feat: Show use icons in app bar button instead of text

--- dorm-app/resources/js/components/AppHeader.vue
+++ dorm-app/resources/js/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-app-bar class="blue darken-4" dense dark app>
-      <v-avatar>
+      <v-avatar class="d-none d-md-flex d-lg-flex d-xl-flex">
         <v-img class="mx-2" :src="avatarurl" max-height="50" max-width="50" contain></v-img>
       </v-avatar>
       <v-toolbar-title class="ml-5 headline" style="font-family: 'Titillium Web' !important;">
@@ -11,19 +11,40 @@
       <v-spacer></v-spacer>
       <v-toolbar-items id="tb-items">
         <v-divider vertical></v-divider>
-        <v-btn text href="/" class="px-4">寮紹介</v-btn>
+        <v-btn text href="/" class="mx-0 px-0" title="寮紹介">
+          <v-icon>mdi-home</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">寮紹介</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
-        <v-btn text href="/personalbook" class="px-4">個人記録</v-btn>
+        <v-btn text href="/personalbook" class="px-0 mx-0"  title="個人記録">
+          <v-icon class>mdi-account-circle</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">個人記録</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
-        <v-btn text href="/dormbook" class="px-4">寮会計</v-btn>
+        <v-btn text href="/dormbook" class="px-0" title="寮会計">
+          <v-icon class>mdi-currency-jpy</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">寮会計</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
-        <v-btn text href="/application" class="px-4">日程管理</v-btn>
+        <v-btn text href="/application" class="px-2" title="日程管理">
+          <v-icon class>mdi-clock</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">日程管理</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
-        <v-btn text href="/userlist" class="px-4">寮生名簿</v-btn>
+        <v-btn text href="/userlist" class="px-0" title="寮生名簿">
+          <v-icon class>mdi-view-list</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">寮生名簿</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
-        <v-btn text href="/archive" class="px-4">寮文書</v-btn>
+        <v-btn text href="/archive" class="px-0" title="寮文書">
+          <v-icon class>mdi-file-document</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">寮文書</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
-        <v-btn text href="/logout" class="px-4">Logout</v-btn>
+        <v-btn text href="/logout" class="px-0" title="ログアウト">
+          <v-icon class>mdi-lock-open</v-icon>
+          <!-- <span class="d-none d-lg-flex d-xl-flex">Logout</span> -->
+        </v-btn>
         <v-divider vertical></v-divider>
       </v-toolbar-items>
     </v-app-bar>
