1ff8cc87f51d414385246de06b16429fb4b715dc
akb <doge.wow.so.scare@gmail.com>
2020-05-18 13:19:40 +0900

    feat: Hide some columns & editing links for non-admin users

--- dorm-app/resources/js/components/member_list/UserTable.vue
+++ dorm-app/resources/js/components/member_list/UserTable.vue
@@ -4,7 +4,20 @@
       <v-card elevation="10">
         <v-card-title>寮生一覧</v-card-title>
         <v-card-text>
-          <v-data-table :headers="userHeaders" :items="users" :items-per-page="20"></v-data-table>
+          <v-data-table :headers="userHeaders" :items="users" :items-per-page="20">
+            <template v-slot:item.name="{item}">
+              <a v-if="isAdmin" :href="editorURL(item.id, 'name')">{{ item.name }}</a>
+              <span v-else>{{ item.name }}</span>
+            </template>
+            <template v-slot:item.room="{item}">
+              <a v-if="isAdmin" :href="editorURL(item.id, 'room')">{{ item.room }}</a>
+              <span v-else>{{ item.room }}</span>
+            </template>
+            <template v-slot:item.role="{item}">
+              <a v-if="isAdmin" :href="editorURL(item.id, 'role')">{{ item.role }}</a>
+              <span v-else>{{ item.role }}</span>
+            </template>
+          </v-data-table>
         </v-card-text>
       </v-card>
     </v-container>
@@ -15,40 +28,53 @@
 export default {
   data: function() {
     return {
+      isAdmin: true,
       users: [],
-      userHeaders: [
-         {
+      userHeadersAll: [
+        {
           text: "ID",
           sortable: true,
-          value: "id"
+          value: "id",
+          isAdminItem: false
         },
-          {
+        {
           text: "名前",
           sortable: true,
-          value: "name"
+          value: "name",
+          isAdminItem: false
         },
         {
           text: "メールアドレス",
           sortable: false,
-          value: "email"
+          value: "email",
+          isAdminItem: true
         },
-         {
-          text: "所属ブロック",
+        {
+          text: "部屋番号",
           sortable: true,
-          value: ""
+          value: "roomNum",
+          isAdminItem: false
         },
-         {
-          text: "部屋番号・移動処理・退寮処理",
+        {
+          text: "役職",
           sortable: true,
-          value: ""
-        },
-         {
-          text: "役職・役職編集",
-          sortable: true,
-          value: ""
-        },
+          value: "role",
+          isAdminItem: false
+        }
       ]
     };
+  },
+  methods: {
+    editorURL(id, type) {
+      return `/edit/${type}/${id}`;
+    }
+  },
+  computed: {
+    userHeaders() {
+      // hide some headers to non-admin users
+      if (this.isAdmin) return this.userHeadersAll;
+      else return this.userHeadersAll.filter(header => !header.isAdminItem);
+    }
   },
   mounted: async function() {
     const res = await axios.get("./users");
