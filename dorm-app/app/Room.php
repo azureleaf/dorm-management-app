66d4767086c02bbd82aca0384de8def81b9b4dd7
akb <doge.wow.so.scare@gmail.com>
2020-05-21 02:33:14 +0900

    feat: Add one-to-one relationships between users & rooms

--- dorm-app/app/Room.php
+++ dorm-app/app/Room.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     protected $table = "rooms";
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
