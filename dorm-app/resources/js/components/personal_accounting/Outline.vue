64623eb1691e2ec2b7bfb18838f36330472e44be
akb <doge.wow.so.scare@gmail.com>
2020-05-28 22:40:56 +0900

    style: Update UI text

--- dorm-app/resources/js/components/personal_accounting/Outline.vue
+++ dorm-app/resources/js/components/personal_accounting/Outline.vue
@@ -2,127 +2,56 @@
   <v-content>
     <v-container>
       <v-card elevation="10">
-        <v-card-title>Dashboard</v-card-title>
+        <v-card-title>納付状況</v-card-title>
         <v-card-text>
-          <v-row>
-            <v-col cols="12" md="4">
-              <v-card height="100%" outlined>
-                <v-toolbar flat class="white--text" color="red darken-4" dark dense>
-                  <v-toolbar-title>
-                    <v-icon class="vmid">mdi-clock</v-icon>
-                    <span class="vmid">行事日程</span>
-                  </v-toolbar-title>
-                </v-toolbar>
-                <v-card-text>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>２F補食室ブロック清掃</v-list-item-title>
-                      <v-list-item-subtitle>2020-03-01 - 2020-03-07</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-list-item-action>
-                      <v-btn fab small color="white">
-                        <v-icon color="red darken-4">mdi-pen</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>寮生大会</v-list-item-title>
-                      <v-list-item-subtitle>2020-03-20</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-list-item-action>
-                      <v-btn fab small color="white">
-                        <v-icon color="red darken-4">mdi-pen</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>脱衣所清掃</v-list-item-title>
-                      <v-list-item-subtitle>2020-03-25</v-list-item-subtitle>
-                    </v-list-item-content>
-                    <v-list-item-action>
-                      <v-btn fab small color="white">
-                        <v-icon color="red darken-4">mdi-pen</v-icon>
-                      </v-btn>
-                    </v-list-item-action>
-                  </v-list-item>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-card height="100%" outlined>
-                <v-toolbar flat class="white--text" color="indigo darken-4" dark dense>
-                  <v-toolbar-title>
-                    <v-icon class="vmid">mdi-currency-jpy</v-icon>
-                    <span class="vmid">引落状況</span>
-                  </v-toolbar-title>
-                </v-toolbar>
-                <v-card-text>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        <v-row align="center">
-                          <v-col>次回の引落予定：</v-col>
-                          <v-col>
-                            <v-chip label outlined large color="indigo darken-4" class="px-4">
-                              <v-icon>mdi-currency-jpy</v-icon>
-                              <span class="headline ml-1">12345</span>
-                            </v-chip>
-                          </v-col>
-                        </v-row>
-                      </v-list-item-title>
-                      <v-list-item-subtitle>
-                        2020-03-01
-                        <br />2020年2月分 + 2020年3月分
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>
-                        <v-row align="center">
-                          <v-col>前回の引落結果：</v-col>
-                          <v-col>
-                            <div v-if="isDebitSuccess" class="px-4">
-                              <v-icon class="vmid" color="green">mdi-check-circle</v-icon>
-                              <span class="vmid">成功</span>
-                            </div>
-                            <div v-else class="px-4">
-                              <v-icon class="vmid">mdi-close-circle</v-icon>
-                              <span class="vmid">失敗</span>
-                            </div>
-                          </v-col>
-                        </v-row>
-                      </v-list-item-title>
-                      <v-list-item-subtitle>
-                        2020-02-10
-                        <br />2020年1月分
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-card height="100%" outlined>
-               <v-toolbar flat class="white--text" color="green darken-4" dark dense>
-                  <v-toolbar-title>
-                    <v-icon class="vmid">mdi-medal</v-icon>
-                    <span class="vmid">賞罰記録</span>
-                  </v-toolbar-title>
-                </v-toolbar>
-                <v-card-text>
-                  <v-list-item two-line>
-                    <v-list-item-content>
-                      <v-list-item-title>風呂掃除放棄罰金 +4000</v-list-item-title>
-                      <v-list-item-subtitle>2020-02分</v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <span>次回の引落予定：</span>
+                <span>
+                  <v-chip label outlined large color="indigo darken-1" class="px-3">
+                    <span class="headline ml-1">{{ formatCurrency(12345)}}</span>
+                  </v-chip>
+                </span>
+              </v-list-item-title>
+              <v-list-item-subtitle>2020-04-20</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <span>前回の納付結果：</span>
+                <span>
+                  <span v-if="isDebitSuccess" class="px-4">
+                    <v-icon class="vmid" color="green">mdi-check-circle</v-icon>
+                    <span class="vmid">引落成功</span>
+                  </span>
+                  <span v-if="isPaidByCash" class="px-4">
+                    <v-icon class="vmid" color="green">mdi-check-circle</v-icon>
+                    <span class="vmid">現金納付</span>
+                  </span>
+                  <span v-else class="px-4">
+                    <v-icon class="vmid" color="red">mdi-close-circle</v-icon>
+                    <span class="vmid">引落失敗</span>
+                  </span>
+                </span>
+              </v-list-item-title>
+              <v-list-item-subtitle>2020-03-20</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-data-table :headers="arrearHeaders" :items="arrears">
+            <template v-slot:item.paid_at="{ item }">
+              <span v-if="item.paid_at != undefined">{{ item.paid_at}}</span>
+              <span v-else>-</span>
+            </template>
+            <template v-slot:item.isNextDebit="{ item }">
+              <v-checkbox
+                v-if="item.status != '引落成功' && item.status != '現金納付'"
+                :disabled="item.status=='次回引落予定'"
+                v-model="isSelected"
+              ></v-checkbox>
+            </template>
+          </v-data-table>
         </v-card-text>
       </v-card>
     </v-container>
@@ -134,32 +63,78 @@ export default {
   data: function() {
     return {
       users: "",
-      isDebitSuccess: true,
-      userHeaders: [
+      selected: [],
+      isSelected: "",
+      isDebitSuccess: false,
+      isPaidByCash: false,
+      arrearHeaders: [
         {
           text: "ID",
           sortable: true,
           value: "id"
         },
         {
-          text: "名前",
+          text: "請求日",
           sortable: true,
-          value: "name"
+          value: "billed_at"
         },
         {
-          text: "メールアドレス",
+          text: "摘要",
           sortable: false,
-          value: "email"
+          value: "abstract"
         },
         {
-          text: "部屋番号",
-          sortable: true,
-          value: ""
+          text: "金額",
+          sortable: false,
+          value: "amount"
         },
         {
-          text: "役職",
-          sortable: true,
-          value: ""
+          text: "状態",
+          sortable: false,
+          value: "status"
+        },
+        {
+          text: "次回引落",
+          sortable: false,
+          value: "isNextDebit"
+        }
+      ],
+      arrears: [
+        {
+          id: 1,
+          billed_at: "2020-04-01",
+          abstract: "2020年３月分",
+          amount: 12345,
+          status: "引落成功",
+          paid_at: "2020-04-20"
+        },
+        {
+          id: 2,
+          billed_at: "2020-04-01",
+          abstract: "2020年３月分",
+          amount: 12345,
+          status: "現金納付"
+        },
+        {
+          id: 3,
+          billed_at: "2020-04-01",
+          abstract: "2020年３月分",
+          amount: 12345,
+          status: "次回引落予定"
+        },
+        {
+          id: 4,
+          billed_at: "2020-04-01",
+          abstract: "2020年３月分",
+          amount: 12345,
+          status: "次回引落予定"
+        },
+        {
+          id: 5,
+          billed_at: "2020-04-01",
+          abstract: "2020年３月分",
+          amount: 12345,
+          status: "滞納中"
         }
       ]
     };
--- dorm-app/resources/views/user-list.blade.php
+++ dorm-app/resources/views/user-list.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <user-table></user-table>
 <room-table></room-table>
-<room-history-table></room-history-table>
+<!-- <room-history-table></room-history-table> -->
 <role-title-table></role-title-table>
 <role-history-table></role-history-table>
 @endsection
