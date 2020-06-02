9ee48ce6878ee1e907703b669df04b7479148a90
akb <doge.wow.so.scare@gmail.com>
2020-06-03 02:10:55 +0900

    chores: Populate seeding samples of billing details & role histories

--- dorm-app/database/seeds/BillingDetailSeeder.php
+++ dorm-app/database/seeds/BillingDetailSeeder.php
@@ -15,27 +15,77 @@ class BillingDetailSeeder extends Seeder
      */
     public function run()
     {
-        $billApr = new Billing;
-        $billApr->user_id = 1;
-        $billApr->closed_at = Carbon::create(2020, 04, 31);
-        $billApr->amount = 0; // temporary value
-        $billApr->save();
+        // Apr, user 1
+        $bill = new Billing;
+        $bill->user_id = 1;
+        $bill->closed_at = Carbon::create(2020, 04, 30);
+        $bill->amount = 0; // temporary value
+        $bill->save();
 
         $detsQuota = new BillingDetail;
-        $detsQuota->billing_id = $billApr->id;
+        $detsQuota->billing_id = $bill->id;
         $detsQuota->personal_account_title_id = PersonalAccountTitle::where('name', '基本金請求')->first()->id;
         $detsQuota->abstract = "４月分基本金請求"; // this must be automatically generated from the params above
         $detsQuota->amount = 15434;
         $detsQuota->save();
 
         $detsPenalty = new BillingDetail;
-        $detsPenalty->billing_id = $billApr->id;
+        $detsPenalty->billing_id = $bill->id;
         $detsPenalty->personal_account_title_id = PersonalAccountTitle::where('name', 'ブロック掃除不履行罰金')->first()->id;
         $detsPenalty->abstract = "４月分ブロック掃除不履行罰金"; // this must be automatically generated from the params above
         $detsPenalty->amount = 2000;
         $detsPenalty->save();
 
-        $billApr->amount = $detsQuota->amount + $detsPenalty->amount;
-        $billApr->save();
+        $bill->amount = $detsQuota->amount + $detsPenalty->amount;
+        $bill->save();
+
+
+        // May, user 1
+        $bill = new Billing;
+        $bill->user_id = 1;
+        $bill->closed_at = Carbon::create(2020, 05, 31);
+        $bill->amount = 0; // temporary value
+        $bill->save();
+
+        $detsQuota = new BillingDetail;
+        $detsQuota->billing_id = $bill->id;
+        $detsQuota->personal_account_title_id = PersonalAccountTitle::where('name', '基本金請求')->first()->id;
+        $detsQuota->abstract = "５月分基本金請求"; // this must be automatically generated from the params above
+        $detsQuota->amount = 19230;
+        $detsQuota->save();
+
+        $detsPenalty = new BillingDetail;
+        $detsPenalty->billing_id = $bill->id;
+        $detsPenalty->personal_account_title_id = PersonalAccountTitle::where('name', '風呂掃除不履行罰金')->first()->id;
+        $detsPenalty->abstract = "５月分風呂掃除不履行罰金"; // this must be automatically generated from the params above
+        $detsPenalty->amount = 4000;
+        $detsPenalty->save();
+
+        $bill->amount = $detsQuota->amount + $detsPenalty->amount;
+        $bill->save();
+
+        // Apr, user 2
+        $bill = new Billing;
+        $bill->user_id = 2;
+        $bill->closed_at = Carbon::create(2020, 04, 30);
+        $bill->amount = 0; // temporary value
+        $bill->save();
+
+        $detsQuota = new BillingDetail;
+        $detsQuota->billing_id = $bill->id;
+        $detsQuota->personal_account_title_id = PersonalAccountTitle::where('name', '基本金請求')->first()->id;
+        $detsQuota->abstract = "４月分基本金請求"; // this must be automatically generated from the params above
+        $detsQuota->amount = 15434;
+        $detsQuota->save();
+
+        $detsReward = new BillingDetail;
+        $detsReward->billing_id = $bill->id;
+        $detsReward->personal_account_title_id = PersonalAccountTitle::where('name', '議長報酬')->first()->id;
+        $detsReward->abstract = "４月分議長報酬"; // this must be automatically generated from the params above
+        $detsReward->amount = -2000;
+        $detsReward->save();
+
+        $bill->amount = $detsQuota->amount + $detsReward->amount;
+        $bill->save();
     }
 }
--- dorm-app/database/seeds/RoleHistoriesTableSeeder.php
+++ dorm-app/database/seeds/RoleHistoriesTableSeeder.php
@@ -17,25 +17,51 @@ class RoleHistoriesTableSeeder extends Seeder
         $boss1 = new App\RoleHistory();
         $boss1->user_id = $userIds[11];
         $boss1->role_title_id = App\RoleTitle::where("name", "委員長")->first()->id;
-        $boss1->start_at = Carbon::create(2020, 01, 01);
-        $boss1->end_at = Carbon::create(2020, 03, 31);
+        $boss1->start_at = Carbon::create(2020, 1, 01);
+        $boss1->end_at = Carbon::create(2020, 3, 31);
         $boss1->reward_pct = 100;
         $boss1->save();
+
+        $boss2 = new App\RoleHistory();
+        $boss2->user_id = $userIds[13];
+        $boss2->role_title_id = App\RoleTitle::where("name", "委員長")->first()->id;
+        $boss2->start_at = Carbon::create(2020, 6, 01);
+        $boss2->end_at = Carbon::create(2020, 9, 30);
+        $boss2->reward_pct = 100;
+        $boss2->save();
+
+        $sub = new App\RoleHistory();
+        $sub->user_id = $userIds[8];
+        $sub->role_title_id = App\RoleTitle::where("name", "副委員長")->first()->id;
+        $sub->start_at = Carbon::create(2020, 6, 01);
+        $sub->end_at = Carbon::create(2020, 9, 30);
+        $sub->reward_pct = 25;
+        $sub->save();
+
+        $director = new App\RoleHistory();
+        $director->user_id = $userIds[10];
+        $director->role_title_id = App\RoleTitle::where("name", "総務")->first()->id;
+        $director->start_at = Carbon::create(2020, 6, 01);
+        $director->end_at = Carbon::create(2020, 9, 30);
+        $director->reward_pct = 25;
+        $director->save();
 
         $accountant1 = new App\RoleHistory();
         $accountant1->user_id = $userIds[14];
         $accountant1->role_title_id = App\RoleTitle::where("name", "会計委員")->first()->id;
-        $accountant1->start_at = Carbon::create(2020, 01, 01);
-        $accountant1->end_at = Carbon::create(2020, 03, 31);
+        $accountant1->start_at = Carbon::create(2020, 6, 01);
+        $accountant1->end_at = Carbon::create(2020, 9, 30);
         $accountant1->reward_pct = 75;
         $accountant1->save();
 
         $accountant2 = new App\RoleHistory();
         $accountant2->user_id = $userIds[18];
         $accountant2->role_title_id = App\RoleTitle::where("name", "会計委員")->first()->id;
-        $accountant2->start_at = Carbon::create(2020, 01, 01);
-        $accountant2->end_at = Carbon::create(2020, 03, 31);
+        $accountant2->start_at = Carbon::create(2020, 6, 01);
+        $accountant2->end_at = Carbon::create(2020, 9, 30);
         $accountant2->reward_pct = 75;
         $accountant2->save();
+
+
     }
 }
