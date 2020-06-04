fe6ff99db65837770e5401311145664a36ac0620
akb <doge.wow.so.scare@gmail.com>
2020-06-05 00:22:25 +0900

    feat: Define global mixin to format JS Date object into YYYY-MM-DD
    YYYY-MM-DD is compatible with PHP Carbon::parse()

--- dorm-app/resources/js/app.js
+++ dorm-app/resources/js/app.js
@@ -18,6 +18,12 @@ Vue.mixin({
                 style: "currency",
                 currency: "JPY"
             }).format(number);
+        },
+        formatDate(date) {
+            var dd = String(date.getDate()).padStart(2, "0");
+            var mm = String(date.getMonth() + 1).padStart(2, "0");
+            var yyyy = date.getFullYear();
+            return `${yyyy}-${mm}-${dd}`;
         }
     }
 });
