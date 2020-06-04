726c9893c0e9dabeff1fcf056c3e5a9ad9c1e7e8
akb <doge.wow.so.scare@gmail.com>
2020-06-04 14:34:33 +0900

    feat: Show deduction table with axios calls
    Temporarily changed rewards can't be shown correctly for now.
    Total persons isn't emitted to parent component for now.

--- dorm-app/resources/js/components/dorm_accounting/closing/DeductionTable.vue
+++ dorm-app/resources/js/components/dorm_accounting/closing/DeductionTable.vue
@@ -2,10 +2,6 @@
   <v-card outlined class="my-5">
     <v-card-title>
       <span>負担人数</span>
-      <!-- <v-spacer></v-spacer>
-      <v-btn color="error" depressed absolute right>
-        <v-icon class="mr-1">mdi-plus-circle</v-icon>負担人数の編集
-      </v-btn> -->
     </v-card-title>
     <v-card-text>
       <!-- <v-card class="pa-3 formula" flat outlined>
@@ -16,11 +12,20 @@
         <v-chip outlined label large color="red">寮費（端数切り上げ）： 12345</v-chip>
       </v-card>-->
       <v-data-table
-        :headers="payerTypeHeaders"
-        :items="payerTypes"
-        :items-per-page="payerTypes.length"
+        :headers="burdenHeaders"
+        :items="burdens"
+        :items-per-page="burdens.length"
         hide-default-footer
-      ></v-data-table>
+      >
+        <template v-slot:item.persons="{ item }">
+          <span :class="{ emphasis: item.isTotalRow }">{{ item.persons }}</span>
+        </template>
+        <template v-slot:item.persons_after_deduction="{ item }">
+          <span :class="{ emphasis: item.isTotalRow }">{{
+            item.persons_after_deduction
+          }}</span>
+        </template>
+      </v-data-table>
     </v-card-text>
   </v-card>
 </template>
@@ -29,6 +34,7 @@
 export default {
   data: function() {
     return {
+      burdens: [],
       payerTypes: [
         {
           type: "委員長",
@@ -103,22 +109,22 @@ export default {
           comment: ""
         }
       ],
-      payerTypeHeaders: [
+      burdenHeaders: [
         {
           text: "職名",
           sortable: false,
-          value: "type"
+          value: "role_name"
         },
-        {
-          text: "開始月",
-          sortable: false,
-          value: "start_at"
-        },
-        {
-          text: "終了月",
-          sortable: false,
-          value: "end_at"
-        },
+        // {
+        //   text: "開始月",
+        //   sortable: false,
+        //   value: "start_at"
+        // },
+        // {
+        //   text: "終了月",
+        //   sortable: false,
+        //   value: "end_at"
+        // },
         {
           text: "負担率",
           sortable: false,
@@ -132,12 +138,7 @@ export default {
         {
           text: "換算負担人数",
           sortable: false,
-          value: "persons_deducted"
-        },
-        {
-          text: "特記事項",
-          sortable: false,
-          value: "comment"
+          value: "persons_after_deduction"
         }
       ]
     };
@@ -147,14 +148,80 @@ export default {
       return this.periods[this.periods.length - 1];
     }
   },
+  methods: {
+    async loadIncumbents() {
+      const res = await axios.get("/role-histories/incumbent");
+      return res.data;
+    },
+    async loadRoleTitles() {
+      const res = await axios.get("/role-titles/");
+      return res.data;
+    },
+    async countCurrentUsers() {
+      const res = await axios.get("/users");
+      const count = res.data.reduce((acc, curr) => {
+        if (curr.move_out_at == null) return ++acc;
+        else return acc;
+      }, 0);
+      return count;
+    },
+    /**
+     * Set burdens[] array which is shown in v-data-table
+     */
+    async setBurdens() {
+      const incumbents = await this.loadIncumbents();
+      const roleTitles = await this.loadRoleTitles();
+      const currentUsersCount = await this.countCurrentUsers();
+
+      // Append committee members to the table
+      roleTitles.forEach(roleTitle => {
+        let personCount = 0;
+        incumbents.forEach(incumbent => {
+          if (roleTitle.id == incumbent.role_title_id) ++personCount;
+        });
+        const burden_rate = 1 - roleTitle.default_reward_pct / 100;
+        this.burdens.push({
+          role_name: roleTitle.name,
+          burden_rate: burden_rate,
+          persons: personCount,
+          persons_after_deduction: personCount * burden_rate
+        });
+      });
+
+      // Append normal boarders to the table
+      const normalUsersCount = currentUsersCount - incumbents.length;
+      this.burdens.push({
+        role_name: "一般寮生",
+        burden_rate: 1,
+        persons: normalUsersCount,
+        persons_after_deduction: normalUsersCount * 1
+      });
+
+      // Append total to the table
+      const personsAfterDeductionTotal = this.burdens.reduce((acc, curr) => {
+        return acc + curr.persons_after_deduction;
+      }, 0);
+      this.burdens.push({
+        role_name: "合計",
+        burden_rate: "",
+        persons: currentUsersCount,
+        persons_after_deduction: personsAfterDeductionTotal,
+        isTotalRow: true
+      });
+    }
+  },
   mounted: async function() {
-    const res = await axios.get("./billings");
-    this.billItems = res.data;
+    this.setBurdens();
   }
 };
 </script>
 <style scoped>
 div.formula > span {
   vertical-align: middle;
+}
+
+.emphasis {
+  font-weight: bold;
+  color: blue;
 }
 </style>
