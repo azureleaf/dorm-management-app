59615a1f0579ee1a042ae8903e49c801cfcce9e7
akb <doge.wow.so.scare@gmail.com>
2020-06-05 01:25:59 +0900

    feat: Show data of closing & approval according to the DB. Doesn't pass prop down while the data is not prepared

--- dorm-app/resources/js/components/dorm_accounting/ClosingTables.vue
+++ dorm-app/resources/js/components/dorm_accounting/ClosingTables.vue
@@ -5,43 +5,34 @@
         <v-card-title>
           <span>{{ feeComp.closing_name }}寮費請求</span>
           <v-spacer></v-spacer>
-          <v-btn
-            color="error"
-            depressed
-            :disabled="hasPendingReport || !isBillingDone"
-          >
+          <v-btn color="error" depressed class="mx-1">
             <v-icon class="mr-1">mdi-plus-circle</v-icon>決算期追加
           </v-btn>
-          <v-btn
-            color="error"
-            depressed
-            :disabled="hasPendingReport || !isBillingDone"
-          >
+          <v-btn color="error" depressed class="mx-1">
             <v-icon class="mr-1">mdi-square-edit-outline</v-icon>決算日設定
           </v-btn>
-          <v-btn
-            color="error"
-            depressed
-            :disabled="!hasPendingReport || isBillingDone"
-          >
+          <v-btn color="error" depressed class="mx-1">
             <v-icon class="mr-1">mdi-security</v-icon>寮費請求
           </v-btn>
         </v-card-title>
         <v-card-text>
           <v-card class="d-flex mt-5 font-weight-bold" flat tile>
             <v-chip label outlined color="green darken-2" class="mr-2" dark>
-              決算日：{{ feeComp.closed_at }}
+              決算日：{{ closingDate }}
             </v-chip>
             <v-chip label outlined color="grey darken-1" class="mr-2">
-              承認日：寮生大会未承認
+              承認日：{{ approvalDate }}
             </v-chip>
           </v-card>
           <monthly-fee
+            v-if="feeComp && personsAfterDeduction"
             :personsprop="personsAfterDeduction"
             :totalamountprop="feeComp.total_amount"
             :feeid="feeComp.id"
           ></monthly-fee>
           <deduction-table
+            v-if="feeComp"
+            :closingdate="feeComp.closed_at"
             @updateDeduction="onDeductionUpdate"
           ></deduction-table>
           <collection-result-table></collection-result-table>
@@ -62,8 +53,6 @@ export default {
   data: function() {
     return {
       personsAfterDeduction: null, // will be calculated by child component
-      hasPendingReport: false,
-      isBillingDone: true,
       fees: []
     };
   },
@@ -72,6 +61,14 @@ export default {
     // don't try to read deep layer values when the fees[] obj isn't loaded yet
     feeComp() {
       return this.fees.length == 0 ? "" : this.fees[0];
+    },
+    closingDate() {
+      return this.feeComp.closed_at ? this.feeComp.closed_at : "決算日未定";
+    },
+    approvalDate() {
+      return this.feeComp.approved_at
+        ? this.feeComp.approved_at
+        : "寮生大会未承認";
     }
   },
   methods: {
