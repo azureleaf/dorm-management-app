14803fc136bdada2c1d183ecd4f90e6a0232f939
akb <doge.wow.so.scare@gmail.com>
2020-06-09 19:13:40 +0900

    chores: Populate billing seeds, bill to everybody

--- dorm-app/database/seeds/BillingDetailSeeder.php
+++ dorm-app/database/seeds/BillingDetailSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\BillingDetail;
 use App\Billing;
+use App\User;
 use App\PersonalAccountTitle;
 use Carbon\Carbon;
 
@@ -15,6 +16,51 @@ class BillingDetailSeeder extends Seeder
      */
     public function run()
     {
+        // List all the users
+        $userIds = App\User::all()->pluck('id')->toArray();
+
+        // Bill the same amount to all the users
+        foreach ($userIds as $userId) {
+
+            $monthlyQuota = array("feb" => 11234, "mar" => 12365);
+
+            /**
+             * Feb
+             */
+            $billFeb = Billing::create([
+                "user_id" => $userId,
+                "closed_at" => Carbon::create(2020, 02, 29),
+                "amount" => $monthlyQuota["feb"],
+            ]);
+            $billFeb->save();
+
+            $detsQuotaFeb =  BillingDetail::create([
+                "billing_id" => $billFeb->id,
+                "personal_account_title_id" => PersonalAccountTitle::where('name', '基本金請求')->first()->id,
+                "abstract" => "２月分基本金請求", // this must be automatically generated from the params above
+                "amount" => $monthlyQuota["feb"],
+            ]);
+            $detsQuotaFeb->save();
+
+            /**
+             * Mar
+             */
+            $billMar = Billing::create([
+                "user_id" => $userId,
+                "closed_at" => Carbon::create(2020, 03, 30),
+                "amount" => $monthlyQuota["mar"],
+            ]);
+            $billMar->save();
+
+            $detsQuotaMar =  BillingDetail::create([
+                "billing_id" => $billMar->id,
+                "personal_account_title_id" => PersonalAccountTitle::where('name', '基本金請求')->first()->id,
+                "abstract" => "３月分基本金請求", // this must be automatically generated from the params above
+                "amount" => $monthlyQuota["mar"],
+            ]);
+            $detsQuotaMar->save();
+        }
+
         // Apr, user 1
         $bill = new Billing;
         $bill->user_id = 1;
