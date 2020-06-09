edb0cd6f902915582245d4959a332c0b7be51bdb
akb <doge.wow.so.scare@gmail.com>
2020-06-09 22:50:21 +0900

    feat: Calculated attribute of readable billing title

--- dorm-app/app/Billing.php
+++ dorm-app/app/Billing.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Billing extends Model
 {
     protected $table = "billings";
-    
+
     public function user()
     {
         return $this->belongsTo('App\User');
@@ -16,5 +17,16 @@ class Billing extends Model
     public function billingDetails()
     {
         return $this->hasMany('App\BillingDetail');
+    }
+
+    protected $appends = [
+        'abstract'
+    ];
+
+    public function getAbstractAttribute()
+    {
+        $closedAt = Carbon::parse($this->closed_at);
+        $month2digits = $closedAt->month > 9 ? $closedAt->month : "0" . strval($closedAt->month);
+        return "{$closedAt->year}年{$month2digits}月分";
     }
 }
