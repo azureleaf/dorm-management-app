29a9f912a58a817b0aed5293acca90560d0a4ea4
akb <doge.wow.so.scare@gmail.com>
2020-06-13 00:15:34 +0900

    fix: UI shows the updated closing date correctly
    Use methods instead of computed property to access sessionStorage

--- dorm-app/resources/js/components/dorm_accounting/ClosingTables.vue
+++ dorm-app/resources/js/components/dorm_accounting/ClosingTables.vue
@@ -25,7 +25,7 @@
             <v-card flat>
               <v-card-text>
                 <v-chip
-                  v-if="draft"
+                  v-if="hasSessionStorage"
                   label
                   outlined
                   color="green darken-2"
@@ -33,7 +33,7 @@
                   large
                   dark
                 >
-                  決算日：{{ draft.closingDate }}
+                  決算日：{{ readDraft().closingDate }}
                 </v-chip>
               </v-card-text>
             </v-card>
@@ -44,14 +44,14 @@
             @updateSS="updateDraftDiff"
           ></monthly-fee>
           <deduction-table
-            v-if="draft"
-            :closingdate="draft.closingDate"
+            v-if="hasSessionStorage"
+            :closingdate="readDraft().closingDate"
             @updateDeduction="onDeductionUpdate"
           ></deduction-table>
           <collection-result-table></collection-result-table>
           <reward-and-penalty-table
-            v-if="draft"
-            :closingdate="draft.closingDate"
+            v-if="hasSessionStorage"
+            :closingdate="readDraft().closingDate"
           ></reward-and-penalty-table>
           <v-btn color="error" block x-large depressed class="mx-1">
             <v-icon class="mr-1">mdi-security</v-icon
@@ -71,35 +71,25 @@ export default {
       persons: { beforeDeduction: null, afterDeduction: null } // will be calculated by child component
     };
   },
-  computed: {
-    // This computed draft behaves as a proxy of the sessionStorage item
-    draft: {
-      get() {
-        return JSON.parse(sessionStorage.getItem("draft"));
-      },
-      set(newDraft) {
-        console.log("setter called! Got:", newDraft);
-        sessionStorage.clear(); // To avoid unexpected collision
-        sessionStorage.setItem("draft", JSON.stringify(newDraft));
-        this.hasSessionStorage = true; // For certainty
-      }
-    }
-  },
   methods: {
     // Handle the emitted values from the deduction component
     onDeductionUpdate(totalPersons) {
       this.persons.beforeDeduction = totalPersons.beforeDeduction;
       this.persons.afterDeduction = totalPersons.afterDeduction;
     },
+    readDraft() {
+      return JSON.parse(sessionStorage.getItem("draft"));
+    },
+    setDraft(newDraft) {
+      sessionStorage.clear(); // To avoid unexpected side-effect
+      sessionStorage.setItem("draft", JSON.stringify(newDraft));
+      this.hasSessionStorage = true; // For certainty
+    },
     createDraft(newClosingDate) {
       const newDraft = {
         closingDate: newClosingDate
       };
-      // Object.assign({}, newDraft); doesn't work. Mysterious. Why???
-      // this.draft = Object.assign(this.draft || {}, newDraft);
-      // this.draft = Object.assign({}, newDraft);
-      // this.draft = JSON.parse(JSON.stringify(newDraft));
-      this.draft = newDraft;
+      this.setDraft(newDraft);
     },
     clearDraft() {
       sessionStorage.clear();
@@ -110,15 +100,13 @@ export default {
      * The other parts will be kept as they are if not affected by the part.
      */
     updateDraftDiff(draftDiff) {
-      let draftCopy = JSON.parse(JSON.stringify(this.draft));
+      let draftCopy = JSON.parse(JSON.stringify(this.readDraft()));
 
       for (let [key, value] of Object.entries(draftDiff)) {
-        // "this.draft[key] = value;" will NOT trigger computed set()
         draftCopy[key] = value;
       }
 
-      // This does trigger the set()
-      this.draft = draftCopy;
+      this.setDraft(draftCopy);
     }
   },
   mounted() {
