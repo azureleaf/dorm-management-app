e1bb7e10209062ec1ae9add536b15ea57fc9c404
akb <doge.wow.so.scare@gmail.com>
2020-06-15 19:02:56 +0900

    feat: Emit the account title ID to the parent component
    e.g. 200 for monthly fee, 105 for chair persion reward
    This title ID is necessary to register new billing items in the parent component

--- dorm-app/resources/js/components/dorm_accounting/closing/RewardAndPenaltyTable.vue
+++ dorm-app/resources/js/components/dorm_accounting/closing/RewardAndPenaltyTable.vue
@@ -92,15 +92,20 @@ export default {
     };
   },
   methods: {
+    /**
+     * Get values emitted from the dialog component,
+     * format them, then push them to the v-data-table arrays
+     *
+     * @param {Obj} rewards Emitted value from the child dialog component
+     */
     onRewardsSet(rewards) {
-      // Get emitted values, format them,
-      // then push them to the v-data-table arrays
       rewards.targetUsers.forEach(targetUser => {
         this.targetUsers.push({
           rowId: ++this.lastRowIndex, // increment then assign. Must be UNIQUE
           userId: targetUser.id,
           fullName: targetUser.full_name,
-          title: rewards.title.name_with_id,
+          title: rewards.title.name_with_id, // e.g. "200: 基本金請求"
+          titleId: rewards.title.id, // e.g. 200
           amount: rewards.amount,
           comment: rewards.comment,
           isDeletable: true
@@ -109,25 +114,36 @@ export default {
 
       this.emitCurrentTargets();
     },
+    /**
+     * Emit the entire rewards / penalty info to the parent component
+     */
     emitCurrentTargets() {
-      // Emit the entire rewards / penalty info to the parent component
       this.$emit("updateRewardAndPenalty", this.targetUsers);
     },
+    /**
+     * Delete the element with the specified ID in the array.
+     * 
+     * @param {Number} rowId Unique (but not always sequential) ID of the element in the targetUsers array
+     */
     deleteRow(rowId) {
-      // Delete the element with the specified ID in the array
-      // Row index is always sequential,
-      // while row IDs may come at intervals
       const rowIndex = this.targetUsers.findIndex(user => {
         return user.rowId == rowId;
       });
       this.targetUsers.splice(rowIndex, 1);
       this.emitCurrentTargets();
     },
-    // Set the rewards to the incumbent users with committee roles
+    /**
+     * Set the rewards to the incumbent users who have committee roles,
+     *  then urge the parent component to store it to the sessionStorage
+     *
+     * @param {Number} monthlyFee
+     *  Amount of monthly fee calculated by the MonthlyFee.vue,
+     *  and will be imposed to all the users
+     */
     setRoleRewards(monthlyFee) {
-      /**
-       * Need to clear all the existing rows of committee roles, but keep other rows
-       */
+      // All the existing rows of committee roles must be cleared,
+      // while other penalties & rewards must be kept.
+      
       // Create the list of row IDs to be deleted
       const oldCommitteeRewardRowIds = this.targetUsers.reduce((acc, row) => {
         if (row.isDeletable == false) {
@@ -141,17 +157,16 @@ export default {
         this.deleteRow(id);
       });
 
-      /**
-       * Calculate the new reward amount
-       * based on the monthly fee & reward ratio
-       */
+      // Calculate the new reward amount
+      //   based on the monthly fee & reward ratio
       this.incumbents.forEach(incumbent => {
         this.targetUsers.push({
           // rowId must be UNIQUE, and the same value mustn't be reused
           rowId: ++this.lastRowIndex, // increment then assign
           userId: incumbent.user.id,
           fullName: incumbent.user.full_name,
-          title: "100: 委員会報酬",
+          title: "100: 委員会報酬", // Hard-coded!
+          titleId: 100, // Hard-coded!
           amount: -Math.ceil(incumbent.reward_pct * 0.01 * monthlyFee),
           comment: `${incumbent.role_title.name}（${incumbent.reward_pct}％免除）`,
           isDeletable: false
@@ -160,10 +175,17 @@ export default {
 
       this.emitCurrentTargets();
     },
+    /**
+     * Axios: Retrieve the list of personal account titles:
+     *   e.g. 委員会報酬、 清掃報酬, 基本金請求, 清掃放棄罰金...
+     */
     async retrieveAccountTitles() {
       const res = await axios.get("./personal/titles");
       this.titles = res.data;
     },
+    /**
+     * Axios: Retrieve the list of users who lived in the month
+     */
     async retrieveUsers() {
       // Parse YYYY-MM-DD into Date object
       const d = new Date(this.closingdate);
@@ -175,8 +197,13 @@ export default {
     }
   },
   watch: {
-    // Detect the update of the props,
-    // then update source array for v-data-table accordingly
+    /**
+     * Detect the update of the props,
+     * then update source array for v-data-table accordingly
+     *
+     * @param {Number} newFee
+     * @param {Number} oldFee
+     */
     monthlyfee(newFee, oldFee) {
       this.setRoleRewards(newFee);
     }
